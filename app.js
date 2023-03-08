d0603b8c1bee0b989e3ea7cd04e2f6918a81cda1
e-aarhol <91724601+e-aarhol@users.noreply.github.com>
2023-03-08 09:41:51 +0100

    Fixed issues with the register function

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const session = require("express-session");
 const bcrypt = require("bcrypt");
 const path = require("path");
 const db = require("better-sqlite3")("mydb.db")
+
 const app = express();
 
 
 
+
 app.use(session({
-    secret: "OmniManIsReal",
+    secret: "Einlangstringsombørveretilfeldiggenerertoglagratdidotenv",
     resave: false,
     saveUninitialized: false //Ved false settes ikke cookie (med sessionID) før en evt gjør endringer i sesjonen
 })) 
@@ -17,14 +19,14 @@ app.use(express.urlencoded({extended: true}))
 
 
 app.get("/regice", (req, res) => {
-    res.sendFile(path.join(__dirname, "/www/regice.html"))
+    res.sendFile(path.join(__dirname, "/regice.html"))
 })
 
 app.get("/", (req, res) => {
     if(req.session.loggedin) {
-        res.sendFile(path.join(__dirname, "/www/index.html"))
+        res.sendFile(path.join(__dirname, "/welcome.html"))
     } else {
-        res.sendFile(path.join(__dirname, "/www/login.html"))
+        res.sendFile(path.join(__dirname, "/login.html"))
     }    
 })
 
@@ -33,7 +35,7 @@ app.post("/login", async (req, res) => {
 
     let userData = db.prepare("SELECT * FROM user WHERE email = ?").get(login.email);
     
-    if(await bcrypt.compare(login.password, userData.hash)) {
+    if(await bcrypt.compare(login.password, userData.password)) {
         req.session.loggedin = true
         res.redirect("/")
     } else {
@@ -44,16 +46,39 @@ app.post("/login", async (req, res) => {
 app.post(("/addUser"), async (req, res) => {
     let svar = req.body;
 
-    let hash = await bcrypt.hash(svar.password, 10)
+    let password = await bcrypt.hash(svar.password, 10) // Fix: Use bcrypt.hash instead of bcrypt.password
     console.log(svar)
-    console.log(hash)
+    console.log(password)
 
-    db.prepare("INSERT INTO user (username, email, password) VALUES (?, ?, ?)").run(svar.name, svar.email, hash)
+    db.prepare("INSERT INTO user (name, email, password) VALUES (?, ?, ?)").run(svar.name, svar.email, password)
     
     res.redirect("back")    
 })
 
 
+
+
+
+app.use(session({
+    secret: "Einlangstringsombørveretilfeldiggenerertoglagratdidotenv",
+    resave: false,
+    saveUninitialized: false //Ved false settes ikke cookie (med sessionID) før en evt gjør endringer i sesjonen
+})) 
+
+app.use(express.urlencoded({extended: true}))
+
+// Eksempel for å vise når en cookie blir satt
+
+app.get("", (req, res) => {
+    console.log(req.session)
+    if (req.session.visits == undefined) {
+        req.session.visits = 1
+    } else {
+        req.session.visits++
+    }
+    
+    res.send("Antall besøkende: " + req.session.visits)
+})
 
 
 
