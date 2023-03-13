59420b51a516f80e27254b4c63cd74c9944394c8
e-aarhol <91724601+e-aarhol@users.noreply.github.com>
2023-03-13 22:53:49 +0100

    Update

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/css.css', (req, res) => {
   });
   
   app.get('/game.js', (req, res) => {
-    res.set('Content-Type', 'text/html');
+    res.set('Content-Type', 'application/javascript');
     res.sendFile(path.join(__dirname, 'game.js'));
   });
 
@@ -45,18 +45,19 @@ app.get("/", (req, res) => {
 
 app.post("/login", async (req, res) => {
     let login = req.body;
-
+  
     let userData = db.prepare("SELECT * FROM user WHERE email = ?").get(login.email);
-    
-    if(await bcrypt.compare(login.password, userData.password)) {
-        req.session.loggedin = true
-        res.redirect("/")
+  
+    if (await bcrypt.compare(login.password, userData.password)) {
+      req.session.loggedin = true;
+      req.session.iduser = userData.iduser; // Set the iduser session variable
+      res.redirect("/");
     } else {
-        res.redirect("back")
+      res.redirect("back");
     }
-})
+  });
 
-app.post(("/addUser"), async (req, res) => {
+  app.post("/addUser", async (req, res) => {
     let svar = req.body;
 
     let password = await bcrypt.hash(svar.password, 10) // Fix: Use bcrypt.hash instead of bcrypt.password
@@ -68,21 +69,21 @@ app.post(("/addUser"), async (req, res) => {
     res.redirect("back")    
 })
 
-
-
-
-
-app.use(session({
-    secret: "Einlangstringsombørveretilfeldiggenerertoglagratdidotenv",
-    resave: false,
-    saveUninitialized: false //Ved false settes ikke cookie (med sessionID) før en evt gjør endringer i sesjonen
-})) 
+app.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
 
 app.use(express.urlencoded({extended: true}))
 
 // Eksempel for å vise når en cookie blir satt
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
     console.log(req.session)
     if (req.session.visits == undefined) {
         req.session.visits = 1
@@ -93,13 +94,96 @@ app.get("", (req, res) => {
     res.send("Antall besøkende: " + req.session.visits)
 })
 
+//rute for admin siden
+app.get("/admin", (req, res) => {
+    let users = db.prepare("SELECT * FROM user").all(); //henter brukere fra databasen
+
+    res.render("admin", { users: users }); //data til bruker sendes til admin.hbs
+});
+
+app.set('view engine', 'hbs'); //express tolker hbs
+app.set('views', __dirname + '/views'); //hbs filer ligger i mappen "views" 
+
+
+app.get('/app.js', (req, res) => {
+    res.setHeader('Content-Type', 'application/javascript');
+    res.sendFile(path.join(__dirname, 'app.js'));
+  });
+
 app.get('/play', (req, res) => {
-    if (!req.session.userId) {
+    if (!req.session.iduser) {
       return res.redirect('/login');
     }
   
-    res.sendFile(path.join(__dirname,  'welcome.html'));
+
+    res.sendFile(path.join(__dirname, 'welcome.html'));
   });
+
+  app.get('/welcome.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'welcome.html'));
+  });
+
+  function updateGameResult(result) {
+    fetch('/update-game-result', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ result })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.message);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+  
+  app.post("/update-game-result", (req, res) => {
+    console.log("Received update game result request");
+    const { result } = req.body;
+    const iduser = req.session.iduser; // Get the iduser session variable
+  
+    if (!iduser) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+  
+    const user = db.prepare("SELECT wins, losses FROM user WHERE iduser = ?").get(iduser);
+  
+    if (result === "win") {
+      db.prepare("UPDATE user SET wins = ? WHERE iduser = ?").run(user.wins + 1, iduser);
+      console.log("Updated wins for user", iduser);
+    } else if (result === "loss") {
+      db.prepare("UPDATE user SET losses = ? WHERE iduser = ?").run(user.losses + 1, iduser);
+      console.log("Updated losses for user", iduser);
+    } else {
+      console.log("Invalid game result");
+    }
+  
+    res.json({ message: "Game result updated" });
+  });
+  
+  app.post("/deleteUser", (req, res) => {
+  const iduser = req.body.iduser;
+
+  db.prepare("DELETE FROM user WHERE iduser = ?").run(iduser);
+
+  res.redirect("/admin");
+});
+  
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- game.js
+++ game.js
@@ -16,10 +16,11 @@ scissorsButton.addEventListener('click', () => {
 });
 
 function playGame(userMove) {
-    console.log
+  console.log('played game')
   const computerMove = generateComputerMove();
 
   if (userMove === computerMove) {
+    console.log('tie')
     setStatus('Tie game!');
   } else if (
     (userMove === 'rock' && computerMove === 'scissors') ||
@@ -27,9 +28,30 @@ function playGame(userMove) {
     (userMove === 'scissors' && computerMove === 'paper')
   ) {
     setStatus('You win!');
+    console.log('winner')
+    updateGameResult('win');
   } else {
     setStatus('Computer wins!');
+    console.log('loser')
+    updateGameResult('loss');
   }
+}
+
+function updateGameResult(result) {
+  fetch('/update-game-result', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ result })
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.message);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 }
 
 function generateComputerMove() {
